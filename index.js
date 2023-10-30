b0fbed39ab191fb61f83c9e4b8094f32608c347f
Vincent Baraga <vincentb.baraga@gmail.com>
2023-10-30 12:21:17 -0400

    added the triangle class and inquirer prompts

--- index.js
+++ index.js
@@ -5,7 +5,40 @@
 const fs = require("fs");
 
 const inquirer = require("inquirer");
+const generateSVG = require("./lib/shapes");
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "shapeType",
+        message: "Choose a shape.",
+        choices: ["Circle", "Rectangle", "Triangle"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "What color would you like your shape to be?",
+      },
+      {
+        type: "input",
+        name: "text",
+        message:
+          "Enter three characters of text you would like your shape to have.",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "What color do you want your text to have?",
+      },
+    ])
+    .then((answers) => {
+      let createdSVG = generateSVG(answers);
+      console.log(createdSVG);
+      fs.writeFile("logo.svg", createdSVG, (err) =>
+        err ? console.error(err) : console.log("Generated logo.svg")
+      );
+    });
+}
 
-fs.writeFile("logo.svg", svgCode, (err) =>
-  err ? console.error(err) : console.log("Success!")
-);
+init();
